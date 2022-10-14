56d38059e413f6744bfb7510d6eccfaf8c53709f
suprugafad <poidaalesya382@gmail.com>
2022-10-15 00:36:39 +0300

    feat: add implementation

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
+const CustomError = require("../extensions/custom-error");
 /**
  * Create transformed array based on the control sequences that original
  * array contains
@@ -13,9 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const out = arr.slice();
+  const discard = Symbol("Discarded item");
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        if (i < out.length - 1) {
+          out[i + 1] = discard;
+        }
+        out[i] = discard;
+        break;
+      case '--discard-prev':
+        if (i > 0) {
+          out[i - 1] = discard;
+        }
+        out[i] = discard;
+        break;
+      case '--double-next':
+        if (i < out.length - 1) {
+          out[i] = out[i + 1];
+        } else {
+          out[i] = discard;
+        }
+        break;
+      case '--double-prev':
+        if (i > 0) {
+          out[i] = out[i - 1];
+        } else {
+          out[i] = discard;
+        }
+    }
+  }
+
+  return out.filter(el => el !== discard);
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
+const CODE_OF_A = 65;
+const LETTERS_IN_ALPHABET = 26;
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -20,13 +21,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type) {
+    this.reverseType = type === false;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt (message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error()
+    }
+
+    let shift = 0;
+    key = key.toUpperCase().split``.map(c => c.charCodeAt(0) - CODE_OF_A);
+    message = message
+        .toUpperCase()
+        .split``
+        .map((item, i) => (/[A-Z]/.test(item))
+            ? String.fromCharCode((item.charCodeAt(0) - CODE_OF_A + key[(i - shift) % key.length])
+                % LETTERS_IN_ALPHABET + CODE_OF_A)
+            : (shift++, item));
+
+    return this.reverseType ? message.reverse().join`` : message.join``;
+  }
+
+  decrypt (message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error()
+    }
+
+    let shift = 0;
+    key = key.toUpperCase().split``.map(c => c.charCodeAt(0) - CODE_OF_A);
+    message = message
+        .toUpperCase()
+        .split``
+        .map((item, i) => (/[A-Z]/.test(item))
+            ? String.fromCharCode((item.charCodeAt(0) - CODE_OF_A + LETTERS_IN_ALPHABET - key[(i - shift) % key.length])
+                % LETTERS_IN_ALPHABET + CODE_OF_A)
+            : (shift++, item));
+
+    return this.reverseType ? message.reverse().join`` : message.join``;
   }
 }
 
